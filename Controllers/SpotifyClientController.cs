a357c470d677d8bfefbf7c20ba7bd1d23272377e
edperkin1 <e.perkin@htempest.co.uk>
2023-10-18 07:52:35 +0100

    Add SpotifyClientController and refactor associated calls
    A new SpotifyClientController has been created to handle Spotify client calls such as playing, pausing, and skipping tracks. The associated calls within the application, specifically within Index.razor, have been refactored to use this new controller, enhancing code organization and maintainability.

---
+++ Controllers/SpotifyClientController.cs
@@ -0,0 +1,42 @@
+﻿using SpotifyAPI.Web;
+
+namespace SpotifyBlazor.Controllers;
+
+public class SpotifyClientController
+{
+    public static bool ActiveDevice;
+    
+    public static bool IsPlaying;
+    
+    public static async Task ChooseDevice(SpotifyClient spotify, Device device)
+    {
+        ActiveDevice = await spotify.Player.TransferPlayback(new PlayerTransferPlaybackRequest(new List<string> { device.Id }));
+    }
+
+    public static async Task Play(SpotifyClient spotify, SimplePlaylist? selectedPlaylist)
+    {
+        await spotify.Player.ResumePlayback(new PlayerResumePlaybackRequest
+        {
+            ContextUri = $"spotify:playlist:{selectedPlaylist.Id}",
+            // OffsetParam = _spotify.Player.GetCurrentlyPlaying(new PlayerCurrentlyPlayingRequest()).Result.Item,
+            PositionMs = spotify.Player.GetCurrentlyPlaying(new PlayerCurrentlyPlayingRequest()).Result.ProgressMs
+        });
+        IsPlaying = true;
+    }
+    
+    public static async Task Pause(SpotifyClient spotify)
+    {
+        await spotify.Player.PausePlayback();
+        IsPlaying = false;
+    }
+
+    public static async Task Next(SpotifyClient spotify)
+    {
+        await spotify.Player.SkipNext();
+    }
+
+    public static async Task Previous(SpotifyClient spotify)
+    {
+        await spotify.Player.SkipPrevious();
+    }
+}
